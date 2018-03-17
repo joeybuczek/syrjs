5062d6a2927af1a4b06a5aab742304bd45d843ea
Joey Buczek <joeybuczek@gmail.com>
2018-03-16 21:44:53 -0400

    corrected title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'syrjs_static',
+    title: 'syrjs-vue',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
